66eb8c20e00e1ad903543bf70edfc188fd72218f
AnhAnNek <info.vananne@gmail.com>
2024-11-03 03:31:27 +0700

    realtime item change

--- src/utils/websocketConstants.js
+++ src/utils/websocketConstants.js
@@ -2,7 +2,13 @@ const notificationTopic = (username) => `/topic/notifications/${username}`;
 
 const notificationDestination = '/app/notifications';
 
+const cartItemCountTopic = (username) => `/topic/cart-item-count/${username}`;
+
+const cartItemCountDestination = '/app/cart-item-count';
+
 export const websocketConstants = {
   notificationTopic,
   notificationDestination,
+  cartItemCountTopic,
+  cartItemCountDestination,
 };
