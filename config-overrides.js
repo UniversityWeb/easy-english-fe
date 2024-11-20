c602f930d6b6bd9aae701b699f29487ee44a7319
AnhAnNek <info.vananne@gmail.com>
2024-11-20 15:15:45 +0700

    fix server error

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,7 @@
-const { override, useBabelRc } = require('customize-cra');
+const { override, useBabelRc, addBabelPlugin } = require('customize-cra');
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
-module.exports = override(useBabelRc());
+module.exports = override(
+  useBabelRc(),
+  addBabelPlugin('@babel/plugin-proposal-private-property-in-object'),
+);
